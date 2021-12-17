4e5c98a126eee9e5243050587362d17b67a51ecd
KevinRodriguez <kevin.rodriguez.mt@correo.usa.edu.co>
2021-12-16 19:41:01 -0500

    reto 1 c4 v1

---
+++ src/main/java/com/c4/reto1/repository/crud/UserCrudRepository.java
@@ -0,0 +1,12 @@
+
+package com.c4.reto1.repository.crud;
+
+import com.c4.reto1.model.User;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UserCrudRepository extends CrudRepository<User, Integer>{
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email, String password);
+}
